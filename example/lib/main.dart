11d610d4624bbab2ac13a11aa80c2ac3a67f3ca3
chenhongchen <412130100@qq.com>
2021-08-21 15:47:18 +0800

    完善例子

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -80,24 +80,24 @@ class _MyAppState extends State<MyApp> {
   Widget _buildCell(BuildContext context, int cellIndex) {
     return GestureDetector(
       onTap: () {
-        // 弹出图片浏览器(单击或下划手势可关闭)
+        // 弹出图片浏览器(默认单击或下划手势可关闭)
         PhotoBrowser(
           itemCount: _bigPhotos.length,
-          initIndex: cellIndex, // 设置初始显示页面索引
+          initIndex: cellIndex,
           controller: _browerController,
-          allowTapToPop: true, // 允许单击关闭
-          allowSwipeDownToPop: true, // 允许向下轻扫关闭
+          allowTapToPop: true,
+          allowSwipeDownToPop: true,
           heroTagBuilder: (int index) {
             return _heroTags[index];
           }, // 飞行动画tag设置，为null则弹出动画为一般的push动画
           imageUrlBuilder: (int index) {
             return _bigPhotos[index];
-          }, // 大图设置，不能为空，如果想本地缓存图片可换imageProviderBuilder属性设置，然后传入带缓存功能的imageProvider
+          }, // 大图设置，如果想本地缓存图片可换imageProviderBuilder属性设置，然后传入带本地缓存功能的imageProvider
           thumImageUrlBuilder: (int index) {
             return _thumPhotos[index];
-          }, // 缩略图设置，可以为空，如果想本地缓存图片可换thumImageProviderBuilder属性设置，然后传入带缓存功能的imageProvider
+          }, // 缩略图设置，如果想本地缓存图片可换thumImageProviderBuilder属性设置，然后传入带本地缓存功能的imageProvider
           positionsBuilder: _positionsBuilder, // 可在图片浏览器上自定义Widget，如关闭按钮、保存按钮
-          loadFailedChild: _failedChild(),
+          loadFailedChild: _failedChild(), // 加载失败
           onPageChanged: (int index) {},
         ).push(
           context,
